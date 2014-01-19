da2e0f8fb6aebcbb71d21233095f88b2d91e6e83
Jeff Squyres <jeff@squyres.com>
2014-01-19 08:40:01 -0600

    Google Analytics track our announcement pageview before redirecting

--- announcement/index.php
+++ announcement/index.php
@@ -1,2 +1,16 @@
-<?php 
-header("Location: http://www.churchofepiphany.com/epiphany/mass-and-prayer");
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN"
+"http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>Church of the Epiphany: Special announcement</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
+<?php include("../include/google_analytics.inc"); ?>
+<script>
+// Once the page has fully loaded (i.e., we sent out Google Analytics 
+// pageview then redirect to the announcement page.  Yay jquery!
+$(document).ready(function(){
+    window.location.replace("http://www.churchofepiphany.com/epiphany/mass-and-prayer");
+});
+</script>
+</head>
+</html>
