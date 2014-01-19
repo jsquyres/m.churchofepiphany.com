87fd79e568c1a616a3e1d42e070e1b17cde21eac
Jeff Squyres <jeff@squyres.com>
2014-01-19 08:40:41 -0600

    Better Google Analytics tracking with events

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-include('include/Mobile_Detect.php');
-$detect = new Mobile_Detect();
+    include('include/Mobile_Detect.php');
+    $detect = new Mobile_Detect();
 ?><!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN"
 "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <title>Church of the Epiphany</title>
 <link type="text/css" rel="stylesheet" href="http://m.churchofepiphany.com/css/master.css" />
-<?php 
-print("<style>");
-include("css/cross-background.css");
-print("</style>\n");
-include("include/google_analytics.inc"); ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
+<style>
+<?php include("css/cross-background.css"); ?>
+</style>
+<?php include("include/google_analytics.inc"); ?>
 </head>
 <body>
 <div id="content">
@@ -20,9 +20,9 @@ include("include/google_analytics.inc"); ?>
     </div>
     <div id="main-links">
         <ul>
-        <!--<li><a href="announcement/" id="main-special-link">Christmas mass times</a></li>-->
-        <li><a href="tel:+15022459733">Call +1 (502) 245-9733</a></li>
-        <li><a href="<?php
+        <!--<li><a id="announcement" href="announcement/" id="main-special-link">Christmas mass times</a></li>-->
+        <li><a id="phonecall" href="tel:+15022459733">Call +1 (502) 245-9733</a></li>
+        <li><a id="maps" href="<?php
     # For iOS, use the maps: URL.  Everyone else (including Android),
     # just use HTTP Google Maps.
     if ($detect->isiOS()) {
@@ -31,14 +31,34 @@ include("include/google_analytics.inc"); ?>
         print("http://goo.gl/maps/3E1RI");
     }
 ?>">Address / map</a></li>
-        <li><a href="liturgies/">Liturgy times</a></li>
-        <li><a href="calendar/">Epiphany calendar</a></li>
+        <li><a id="liturgies" href="liturgies/">Liturgy times</a></li>
+        <li><a id="calendar" href="calendar/">Epiphany calendar</a></li>
         </ul>
      <div id="push"></div>
     </div>
 </div>
 <div id="footer">
-    <a href="http://www.churchofepiphany.com/?nomobi=true">View full website</a>
+    <a id="fullwebsite" href="http://www.churchofepiphany.com/?nomobi=true">View full website</a>
 </div>
+<script>
+$('#announcement').click(function(){
+    ga('send','event','Special announcement','click', 'Tapped special announcement button');
+});
+$('#phonecall').click(function(){
+    ga('send','event','Call','click', 'Tapped Call button');
+});
+$('#maps').click(function(){
+    ga('send','event','Maps','click', 'Tapped Address/map button');
+});
+$('#liturgies').click(function(){
+    ga('send','event','Liturgies','click', 'Tapped Liturgies button');
+});
+$('#calendar').click(function(){
+    ga('send','event','Calendar','click', 'Tapped Calendar button');
+});
+$('#fullwebsite').click(function(){
+    ga('send','event','Full site','click', 'Tapped View full website button');
+});
+</script>
 </body>
 </html>
